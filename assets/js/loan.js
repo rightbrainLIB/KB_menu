83788dbe1d77b5f16f82ce027ab87cf40bf56e05
RByunsu <yss@rightbrain.co.kr>
2024-01-18 13:43:07 +0900

    전체메뉴명 수정, 대출 페이지 수정, 대출관리 페이지 추가

--- assets/js/loan.js
+++ assets/js/loan.js
@@ -3,10 +3,18 @@ const back = () => {
 	const menuBtn = document.querySelector(".menu-btn");
 	
 	backBtn.addEventListener("click", () => {
-		window.history.go(-1) 
+		if(document.referrer.includes('typeC_loan.html')) {
+			window.history.go(-2) 
+		} else {
+			window.history.go(-1) 
+		}
 	})
 	menuBtn.addEventListener("click", () => {
-		window.history.go(-1) 
+		if(document.referrer.includes('typeC_loan.html')) {
+			window.history.go(-2) 
+		} else {
+			window.history.go(-1) 
+		}
 	})
 }
 
@@ -30,29 +38,55 @@ const tab = () => {
 
 const sheetOpen = () => {
 	const dim = document.querySelector(".dim");
-	const sheet = document.querySelector(".bottom-sheet");
-	const moreBtn = document.querySelector(".more-btn.sheet");
-	const closeBtn = document.querySelector(".close-btn");
+	const sheet01 = document.querySelector(".bottom-sheet.sheet01");
+	const sheet02 = document.querySelector(".bottom-sheet.sheet02");
+	const moreBtn = document.querySelectorAll(".more-btn");
+	const closeBtn = document.querySelectorAll(".close-btn");
 	const body = document.querySelector("body");
-	moreBtn.addEventListener("click", () => {
-		dim.classList.add("open")
-		sheet.classList.add("open")
-		body.classList.add("scroll-block")
+	
+	moreBtn.forEach((item) => {
+		item.addEventListener("click", (e) => {
+			if (item.classList.contains("sheet01")) {
+				sheet01.classList.add("open")
+			}if (item.classList.contains("sheet02")) {
+				sheet02.classList.add("open")
+			}
+			dim.classList.add("open")
+			body.classList.add("scroll-block")
+		})
 	})
-	closeBtn.addEventListener("click", () => {
-		dim.classList.remove("open")
-		sheet.classList.remove("open")
-		body.classList.remove("scroll-block")
+	closeBtn.forEach((item) => {
+		item.addEventListener("click", (e) => {
+			item.closest(".bottom-sheet").classList.remove("open")
+			dim.classList.remove("open")
+			body.classList.remove("scroll-block")
+		})
 	})
 	dim.addEventListener("click", () => {
 		dim.classList.remove("open")
-		sheet.classList.remove("open")
+		sheet01.classList.remove("open")
+		sheet02.classList.remove("open")
 		body.classList.remove("scroll-block")
+	})
+}
+
+const detailView = () => {
+	const detailViewBtn = document.querySelector(".detail-view-btn");
+	const detail = document.querySelector(".detail-view");
+
+	detailViewBtn.addEventListener("click", (e) => {
+		detailViewBtn.classList.toggle("active");
+		if (e.target.classList.contains("active")) {
+			detail.style.maxHeight = detail.scrollHeight + "px";
+		} else {
+			detail.style.maxHeight = 0;
+		}
 	})
 }
 
 document.addEventListener("DOMContentLoaded", () => {
   back();
 	tab();
-	sheetOpen()
+	sheetOpen();
+	detailView();
 });
